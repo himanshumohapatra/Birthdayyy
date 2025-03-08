c48503517c25c2c343d4304400eb975cd756c18c
himanshumohapatra <himanshumohapatra2005@gmail.com>
2025-03-08 21:59:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Example of showing an alert after the form submission
 const form = document.querySelector('form');
 form.addEventListener('submit', function() {
-  alert('Form submitted successfully! Check your email for the results.');
+  alert('Form submitted successfully!');
 });
